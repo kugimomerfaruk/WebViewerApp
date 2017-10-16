e0ea122c5fcb9053702920c197e8a3d4bf261297
Omer Faruk <kugimomerfaruk@gmail.com>
2017-10-16 09:20:00 +0300

    Initial Commit

---
+++ WebApp2Tests/WebApp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  WebApp2Tests.swift
+//  WebApp2Tests
+//
+//  Created by ST1 on 16.10.2017.
+//  Copyright © 2017 Kugim. All rights reserved.
+//
+
+import XCTest
+@testable import WebApp2
+
+class WebApp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
